4efad419f42a5d5f3fe9fc01a624640c44f6702b
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-08-21 02:45:02 -0400

    introduce simple scale-shift concentric funtion. Data multiplicity count

---
+++ py-gen/src/sculptures/concentric.py
@@ -0,0 +1,26 @@
+from src.functions.scale import Scale
+from src.functions.translate import Translate
+from src.functions.composition import Composition
+from src.functions.copy import Repeat
+
+from src.helpers.zipapply import ZipApply
+
+from src.atoms import dimension
+
+from src.typeclass.__sculpture__ import __Sculpture__
+
+from numpy import array
+
+def Concentric(sculpture, count):
+    sculpture = sculpture.sculpt()
+    dim = dimension(sculpture)
+
+    funcs = []
+    for comp in range(count + 1):
+        funcs = [ Composition( [ Scale(comp/count)
+                               , Translate(array([1 - comp/count for _ in range(dim)]))
+                               ]
+                             )
+                , *funcs
+                ]
+    return __Sculpture__(__Sculpture__(sculpture, Repeat(count)).sculpt(), ZipApply(funcs))
