301bf7163b2d35986c34bc98e4d769f6a68bbaf9
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-08-22 16:51:33 -0400

    work on 'smoothing' function for beziers collapse_to

--- py-gen/src/helpers/numpy/__init__.py
+++ py-gen/src/helpers/numpy/__init__.py
@@ -1,4 +1,4 @@
-from numpy import array_split, concatenate
+from numpy import array_split, concatenate, square
 
 def linear_interpolate(A, collapse_axes, samples):
     for axis, sample in zip(collapse_axes, samples):
@@ -20,6 +20,31 @@ def linear_interpolate(A, collapse_axes, samples):
         A = concatenate(C, axis=axis)
     return A
 
-def populate_average_tangents(A):
-    pass
+def populate_average_tangents(A, collapse_to):
+    for axis in [ax for ax in range(len(A.shape)) if ax != collapse_to]:
+        B = array_split(A, A.shape[axis], axis)
 
+        E = []
+        while True:
+            match B:
+                case [] | [_] | [_,_]:
+                    break
+                case [a, b, c]:
+                    # This heuristic is reasonable in the instance A is (n, m)
+                    # but falls apart for beyond (n, m, ...). Perhaps we should
+                    # capture the poles along collapse_to axis and scale c - a 
+                    # w.r.t. projection length. i.e. (c - a) * (c - b) dot (c - a)
+                    # This would make a rectangular type form over the five points.
+                    e = c - a / square(c - a).sum()
+                    print(b - e, b, b + e)
+                    E = [*E, b - e, b, b + e]
+                    break
+                case [a, b, c, *B]:
+                    e = c - a / square(c - a).sum()
+                    print(b - e, b, b + e)
+                    E = [*E, b - e, b, b + e]
+                    B = [b, c, *B]
+
+        a, *B, c = B
+        A = concatenate([a, *E, c], axis=axis)
+    return A
