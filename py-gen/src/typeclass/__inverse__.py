2424b0705fcf17bee3261f51ec922a28ae1d7259
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-09-01 04:19:22 -0400

    introduce __Random__ and __Inverse__ Meta. Functions which are capable should be implemented

---
+++ py-gen/src/typeclass/__inverse__.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+
+class __Inverse__(ABC):
+    @abstractmethod
+    def inverse(self):
+        rasie NotImplementedError
---
+++ py-gen/src/typeclass/__random__.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+
+class __Random__(ABC):
+    @abstractmethod
+    def random(self):
+        rasie NotImplementedError
