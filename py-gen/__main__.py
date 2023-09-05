2cc8725b518b2c4cccbb7c5bb5de54c53386c678
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-09-05 15:52:42 -0400

    U.06 coming along nicely

--- py-gen/__main__.py
+++ py-gen/__main__.py
@@ -16,7 +16,8 @@ from numpy.random import rand
 from math import pi
 
 from src.functions import Parallelogram, Bezier, Sphere, Dialate, \
-        Translate, Composition, ID, Ball, Perlin_Noise, Perlin_Stack, Perlin_Vector, Add
+        Translate, Composition, ID, Ball, Perlin_Noise, Perlin_Stack, \
+        Perlin_Vector, Add, AccumulateOnto, Copy
 from src.sculptures import FlexHyperCube, FlexCube, FlexPlane, \
         UnitLine, Cube, Square, HCube, TemporalFrameBezierNoise
 from src.atoms import Point, Segment, Triangle, draw, wrap, write_to_file, List
@@ -25,26 +26,36 @@ from src.helpers.numpy import *
 from mp.pose_landmark_detection import Pose_Landmark_Detection
 from src.typeclass.__sculpture__ import __Sculpture__
 
-# Add is not configured properly. I want a proper __call__ funtion which takes the existing data and adds
-# onto it the valuation of another function atop it. AccumulateOnto?
+# Remember to do make the sculpture that makes a set of beziers that reduce order in order. Update each to upgrade
+# to the max order. What's more, add multithreading to rendering processes. Functions may or may not be seperable
+# into component pieces.
+
 def explore():
-    for k in range(20):
-        A = rand(10, 4)
+    for k in range(10):
+        A = rand(10, 3)
         print(k, A)
-        f = lambda A: __Sculpture__( UnitLine(500).sculpt()
+
+        f = lambda A, noise, deformation: __Sculpture__( UnitLine(1500).sculpt()
                                     , Composition([
-                                     Add( Perlin_Vector.random()(4)
-                                        , Composition(
+                                          Composition(
                                             [ Bezier()(A, [0])
-                                            , Parallelogram(array([[1,0,0,0],[0,2*pi,0,0],[0,0,pi,0],[0,0,0,pi]]))
-                                            , Translate(array([1,0,0,0]))
+                                            , Parallelogram(array([[1,0,0],[0,2*pi,0],[0,0,pi]]))
+                                            , Translate(array([1,0,0]))
                                             , Ball()
                                             ])
-                                       )
-                                    , Parallelogram(array([[1,0,0,0], [0,0,0,1]]))
+                                        , AccumulateOnto(deformation, .0075)
+                                        , AccumulateOnto(noise, .0025)
+                                        , Parallelogram(array([[1,0,0], [0,0,1]]))
                                     ])
                                     ).sculpt()
-
+        deform = Composition([ Bezier()(rand(3,5,5,5),[3,2,1])
+                             , Parallelogram(array([[1,0,0],[0,2*pi,0],[0,0,pi]]))
+                             , Ball()
+                             ])
+        noise = Perlin_Vector.random()(3)
         N = [make_closed_LNE(populate_MVT(A, 1, 2, i*.0125), 0, .5)  for i in range(2, 60)]
-        L = List(list(map(f, N)))
-        write_to_file(f"smoothtest_{k+640}.svg", wrap(draw(L)))
+        L = List(list(map(lambda A: f(A, noise, deform), N)))
+
+        write_to_file(f"smoothtest_{k+750}.svg", wrap(draw(L)))
+
+## Line/Circle -> SumOfSpheres -> Bezier -> Concentric Circles / Squares
