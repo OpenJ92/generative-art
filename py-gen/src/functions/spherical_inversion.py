2913877fc8bd7684f7f2778a83179d9908a07e3d
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-09-19 09:20:47 -0400

    implement spherical inversion

--- py-gen/src/functions/spherical_inversion.py
+++ py-gen/src/functions/spherical_inversion.py
@@ -3,13 +3,16 @@ from numpy import array, square
 from src.typeclass.__function__ import __Function__
 from src.typeclass.__random__ import __Random__
 
+
 class SphereInversion(__Random__, __Function__):
     def __init__(self, center, radius):
         self.center = center
         self.radius = radius
 
     def __call__(self, data: array):
-        pass
+        diff = data - self.center
+        length_diff = sum(square(diff))
+        return self.center + (self.radius**2 * (diff / length_diff))
 
     @classmethod
     def random(self):
