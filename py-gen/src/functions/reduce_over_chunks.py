8a72dc637392b907c986d14b13a567de073b0601
OpenJ92 <jacob.vartuli.92@gmail.com>
2025-06-05 07:12:45 -0400

    update ROC strict __call__

--- py-gen/src/functions/reduce_over_chunks.py
+++ py-gen/src/functions/reduce_over_chunks.py
@@ -3,7 +3,7 @@ from src.typeclass import Function
 class ReduceOverChunks(Function):
     def __init__(self, size, scales):
         self.size = size
-        self.scale = scale
+        self.scales = scales
 
     def __call__(self, data : array):
         ## retrieve data from Data and chunck into some 'chunk_size'
@@ -12,9 +12,8 @@ class ReduceOverChunks(Function):
         ## is in size of 2 and scale of the square of elements of a 
         ## hypersphere. Perhaps we'll 'realize' that form here for use
         ## elsewhere. 
-        chunks = []
+        output = zeros((self.size))
         for index in range(0, len(array), size):
-            point = data[index:index+size]
-            chunks.append(Point(l=point)
+            output += scales[index//self.size]*data[index:index+size]
 
-        return List(chunks)
+        return output
