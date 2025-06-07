6fc6c5154cbb146f6bcfd53a24aeef9b43c7bd38
OpenJ92 <jacob.vartuli.92@gmail.com>
2025-06-07 07:06:11 -0400

    Concentric impl incorrect.

--- py-gen/src/functions/reduce_over_chunks.py
+++ py-gen/src/functions/reduce_over_chunks.py
@@ -5,6 +5,19 @@ class ReduceOverChunks(Function):
         self.size = size
         self.scales = scales
 
+    ## def __call__(self, data : array):
+    ##     ## retrieve data from Data and chunck into some 'chunk_size'
+    ##     ## arrays and pad as needed. then take the dot product with
+    ##     ## provided array of scales. The implementation I'm considering
+    ##     ## is in size of 2 and scale of the square of elements of a 
+    ##     ## hypersphere. Perhaps we'll 'realize' that form here for use
+    ##     ## elsewhere. 
+    ##     output = zeros((self.size))
+    ##     for index in range(0, len(array), size):
+    ##         output += scales[index//self.size]*data[index:index+size]
+
+    ##     return output
+
     def __call__(self, data : array):
         ## retrieve data from Data and chunck into some 'chunk_size'
         ## arrays and pad as needed. then take the dot product with
@@ -12,8 +25,8 @@ class ReduceOverChunks(Function):
         ## is in size of 2 and scale of the square of elements of a 
         ## hypersphere. Perhaps we'll 'realize' that form here for use
         ## elsewhere. 
-        output = zeros((self.size))
+        output = []
         for index in range(0, len(array), size):
-            output += scales[index//self.size]*data[index:index+size]
+            output.append(Point(data[index:index+size]))
 
         return output
